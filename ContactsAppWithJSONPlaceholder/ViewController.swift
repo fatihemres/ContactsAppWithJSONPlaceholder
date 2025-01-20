995d2ec998b7caf14fcd6c64c63e7b724186e055
Fatih Emre <112892044+blaineconnor@users.noreply.github.com>
2025-01-20 08:11:39 +0300

    Initial Commit

---
+++ ContactsAppWithJSONPlaceholder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsAppWithJSONPlaceholder
+//
+//  Created by Fatih Emre on 20.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

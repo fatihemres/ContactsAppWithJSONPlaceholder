b83bd0e55b7069f68fd0190a458fc00789926017
Fatih Emre <112892044+blaineconnor@users.noreply.github.com>
2025-01-21 16:41:38 +0300

    feat: Package dependencies included the project with cocoapods.

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '14.0'
+use_frameworks!
+
+target 'ContactsAppWithJSONPlaceholder' do
+    pod 'Then'
+    pod 'SnapKit'
+    pod 'CombineCocoa'
+end
---
+++ Pods/Target Support Files/Pods-ContactsAppWithJSONPlaceholder/Pods-ContactsAppWithJSONPlaceholder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ContactsAppWithJSONPlaceholder : NSObject
+@end
+@implementation PodsDummy_Pods_ContactsAppWithJSONPlaceholder
+@end
